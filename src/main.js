6ea99f7fec42ae5c56c9e87212b1d9f82d71658b
Liz <liz-delrio@outlook.com>
2021-07-20 18:15:51 -0500

    se agregan #pokedex y tipo/elemnto dinamicamente al DOM

--- src/main.js
+++ src/main.js
@@ -18,16 +18,37 @@ data.pokemon.forEach((item, i) => {
   //se crea un div por cada pokemon
   let div = document.createElement('div');
   div.className = 'poke-div';
+  //Se crea el div q tendra las info pokemon
+  let divInfo = document.createElement('div');
+  divInfo.className = 'info-div';
+  //div que contendra la info de tipo/elemnto pokemon
+  let divTyp = document.createElement('div');
+  divTyp.className = 'poke-type';
+  //se crean los p que tendran la info tipo/elemnto pokemon
+  let pTyp1 = document.createElement('p');
+  pTyp1.className = 'p-type'
+  //let pTip2 = document.createElement('p');
+  //Se crea la p q contendra el # de pokedex
+  let pNum = document.createElement('p');
+  pNum.className = 'p-num'
   //Se crea el h2 q contendra el nombre del pokemon
   let h2 = document.createElement('h2');
   //Se crea la img q contendra la imagen
-  let img = document.createElement('img')
+  let img = document.createElement('img');
   //se obtiene el nombre, numero e imagen de la data
+  pNum.textContent = '# ' + item.num;
+  pTyp1.textContent = item.type;
+  //pTip2.textContent = item.type;
   h2.textContent = item.name;
   img.src = item.img;
   img.alt = "imagen" + item.name;
   //se agrega cada div al index.html
   list.appendChild(div);
+  div.appendChild(divInfo);
+  divInfo.appendChild(pNum);
+  divInfo.appendChild(divTyp);
+  divTyp.appendChild(pTyp1);
+  //divTip.appendChild(pTip2);
   div.appendChild(img);
   div.appendChild(h2);
   console.log("Nombre poke: " + item.name + " " + i);
