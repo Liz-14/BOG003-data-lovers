820bc829af7c3c0fa11a6b461674dd263d9b39eb
Liz <liz-delrio@outlook.com>
2021-07-29 16:51:36 -0500

    Se documenta el codigo

--- src/data.js
+++ src/data.js
@@ -2,20 +2,24 @@
 export const sortData = (data, sortBy, sortOrder) => {
   let newData = [];
   if (sortBy === "name"){
+    //ordena de la A a la Z
     data.sort((a,b) => {
       //------operador terniario-------
       //(condicion) ? resultado_cierto : resultado_falso
       return (a.name < b.name) ? -1 : 1;
       });
-
+      //Ordenar por grupos alfabeticos para el navBar
       if(sortOrder.length === 4){
         data.forEach((item, i) => {
+          /*startsWith(): busca en el array el elemento que comience por la
+          letra/parametro que se le indique*/
+          /*charAt(): permite tomar cada caracter de un string segun su posicion */
             if(item.name.startsWith(sortOrder.charAt(0)) ||
             item.name.startsWith(sortOrder.charAt(1)) ||
             item.name.startsWith(sortOrder.charAt(2)) ||
             item.name.startsWith(sortOrder.charAt(3))){
               newData.push(data[i]);
-            };
+            }
           });
         }
         else {
@@ -24,17 +28,9 @@ export const sortData = (data, sortBy, sortOrder) => {
               item.name.startsWith(sortOrder.charAt(1)) ||
               item.name.startsWith(sortOrder.charAt(2))){
                 newData.push(data[i]);
-              };
+              }
           });
         }
-
-
     return newData;
   }
 };
-
-
-
-/*export const anotherExample = () => {
-  return 'OMG';
-};*/
