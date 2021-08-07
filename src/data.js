5f20f7043542dabb01fef1b9b3d0da4bc92aaa64
Liz <liz-delrio@outlook.com>
2021-08-06 20:17:18 -0500

    se le agrega funcionalidad al select

--- src/data.js
+++ src/data.js
@@ -23,47 +23,66 @@ export const sortData = (data, sortBy, sortOrder) => {
       });
       //Ordenar por grupos alfabeticos para el navBar
       if(sortOrder.length === 4){
-        data.forEach((item, i) => {
-          /*startsWith(): busca en el array el elemento que comience por la
-          letra/parametro que se le indique*/
-          /*charAt(): permite tomar cada caracter de un string segun su posicion */
-            if(item.name.startsWith(sortOrder.charAt(0)) ||
-            item.name.startsWith(sortOrder.charAt(1)) ||
-            item.name.startsWith(sortOrder.charAt(2)) ||
-            item.name.startsWith(sortOrder.charAt(3))){
-              newData.push(data[i]);
-            }
-          });
+        /*startsWith(): busca en el array el elemento que comience por la
+        letra/parametro que se le indique*/
+        /*charAt(): permite tomar cada caracter de un string segun su posicion */
+        newData = data.filter(el => el.name.startsWith(sortOrder.charAt(0)) ||
+                                    el.name.startsWith(sortOrder.charAt(1)) ||
+                                    el.name.startsWith(sortOrder.charAt(2)) ||
+                                    el.name.startsWith(sortOrder.charAt(3)));
         }
         else {
-          data.forEach((item, i) => {
-              if(item.name.startsWith(sortOrder.charAt(0)) ||
-              item.name.startsWith(sortOrder.charAt(1)) ||
-              item.name.startsWith(sortOrder.charAt(2))){
-                newData.push(data[i]);
-              }
-          });
+          newData = data.filter(el => el.name.startsWith(sortOrder.charAt(0)) ||
+                                      el.name.startsWith(sortOrder.charAt(1)) ||
+                                      el.name.startsWith(sortOrder.charAt(2)));
         }
   }
 
-
   //Busqueda por nombre
   if (sortBy === "searchName") {
-    data.forEach((item, i) => {
-      if (item.name.includes(sortOrder)) {
-        newData.push(data[i]);
-      }
-    });
+    //array.filter(elemnto => condicion)
+    newData = data.filter(el => el.name.includes(sortOrder.toLowerCase()));
+  }
+
+  //ordenar de mayor a menor las estadísticas base
+  if (sortBy === "basic-stats") {
+    newData = computeStats(data);
+    newData.sort((a,b) => {
+      return (a[sortOrder] < b[sortOrder]) ? 1 : -1;
+      });
   }
   return newData;
 };
 
+//Recorre todo el array hasta encontrar el elemento que cumple la condicion
 export const filterData = (data, condition) => {
-  let newData = [];
-  data.forEach((item) => {
-    if (condition == item.name) {
-      newData.push(item);
-    }
+  let newData = data.filter(el => el.name == condition);
+  return newData;
+}
+
+export const computeStats = (data) => {
+  let basicStats = data.map(item => {
+    let attack = Number(item.stats["base-attack"]);
+    let defense = Number(item.stats["base-defense"]);
+    let stamina = Number(item.stats["base-stamina"]);
+   return  [attack, defense, stamina];
   });
+
+  let sum = basicStats.map(item => {
+   return  item.reduce((a, b) => a + b, 0);
+  });
+
+  let average = sum.map(item => {
+   return  item / 3;
+  });
+
+  let newData = data.map((item, i) => {
+    sum.map(element => {
+      item['sum'] = sum[i];
+      item['average'] = average[i].toFixed(2);
+    })
+    return item
+  });
+
   return newData;
 }
